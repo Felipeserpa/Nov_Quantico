1023273b0882b2154c0b02296cfbf4eaefeff131
Felipe serpa <felipe.mago_skt@hotmail.com>
2025-04-08 18:58:55 -0300

    implementaçao da pagina de serviço

--- src/sections/Skills/index.tsx
+++ src/sections/Skills/index.tsx
@@ -22,56 +22,75 @@ export default function Skills() {
           <div className="section-container">
             <div id="skills-container" data-aos="fade-up">
               <div className="skill-card">
-                <h2>Git</h2>
-                <FaGitAlt className="icon" />
+                <h2>Criação de Identidade visual</h2>
+                <p>
+                  Transformamos a essência da sua marca em uma linguagem visual
+                  única e memorável. Do logotipo à paleta de cores, criamos uma
+                  identidade que conecta e destaca seu negócio no mercado.
+                </p>
+                <a href="/servico-identidade-visual" className="button">
+                  Saiba Mais
+                </a>
               </div>
               <div className="skill-card">
-                <h2>JavaScript</h2>
-                <FaJs className="icon" />
+                <h2>Fachadas comercial</h2>
+                <p>
+                  Projetamos fachadas que atraem e impressionam, criando um
+                  ponto de contato visual impactante para o seu negócio.
+                  Utilizamos materiais de alta qualidade e designs inovadores
+                  para garantir que sua fachada se destaque.
+                </p>
+                <a href="/servico-identidade-visual" className="button">
+                  Saiba Mais
+                </a>
               </div>
               <div className="skill-card">
-                <h2>TypeScript</h2>
-                <SiTypescript className="icon" />
+                <h2>Placas e Totens Publicitários</h2>
+                <p>
+                  Desenvolvemos placas e totens publicitários que comunicam sua
+                  mensagem de forma clara e eficaz. Nossas soluções são
+                  personalizadas para maximizar a visibilidade da sua marca em
+                  qualquer ambiente.
+                </p>
+                <a href="/servico-identidade-visual" className="button">
+                  Saiba Mais
+                </a>
               </div>
               <div className="skill-card">
-                <h2>React</h2>
-                <FaReact className="icon" />
+                <h2>Adesivação de Ambiente e Veiculos</h2>
+                <p>
+                  Transforme seus espaços e veículos em poderosas ferramentas de
+                  marketing. Oferecemos soluções de adesivação que combinam
+                  design criativo e materiais duráveis, garantindo um impacto
+                  visual duradouro.
+                </p>
+                <a href="/servico-identidade-visual" className="button">
+                  Saiba Mais
+                </a>
               </div>
               <div className="skill-card">
-                <h2>React Native</h2>
-                <TbBrandReactNative className="icon" />
+                <h2>Sinalização Interna e Externa</h2>
+                <p>
+                  Criamos sistemas de sinalização que guiam, informam e
+                  organizam, melhorando a experiência do usuário em qualquer
+                  ambiente. Nossas soluções são projetadas para atender às
+                  necessidades específicas do seu negócio.
+                </p>
+                <a href="/servico-identidade-visual" className="button">
+                  Saiba Mais
+                </a>
               </div>
               <div className="skill-card">
-                <h2>Next.js</h2>
-                <TbBrandNextjs className="icon" />
-              </div>
-              <div className="skill-card">
-                <h2>Node.js</h2>
-                <FaNodeJs className="icon" />
-              </div>
-              <div className="skill-card">
-                <h2>MongoDB</h2>
-                <SiMongodb className="icon" />
-              </div>
-              <div className="skill-card">
-                <h2>Postgresql</h2>
-                <SiPostgresql className="icon" />
-              </div>
-              <div className="skill-card">
-                <h2>Styled Components</h2>
-                <SiStyledcomponents className="icon" />
-              </div>
-              <div className="skill-card">
-                <h2>Tailwindcss</h2>
-                <SiTailwindcss className="icon" />
-              </div>
-              <div className="skill-card">
-                <h2>Figma</h2>
-                <SiFigma className="icon" />
-              </div>
-              <div className="skill-card">
-                <h2>Firebase</h2>
-                <SiFirebase className="icon" />
+                <h2>Custom designs</h2>
+                <p>
+                  Desenvolvemos projetos personalizados que refletem a
+                  individualidade da sua marca. Do conceito à execução, nossa
+                  equipe trabalha para criar soluções únicas que superam suas
+                  expectativas.
+                </p>
+                <a href="/servico-identidade-visual" className="button">
+                  Saiba Mais
+                </a>
               </div>
             </div>
           </div>
--- src/sections/Skills/styles.ts
+++ src/sections/Skills/styles.ts
@@ -3,47 +3,64 @@ import styled from 'styled-components';
 const Container = styled.div`
   #skills-container {
     max-width: 1100px;
-    display: flex;
-    justify-content: center;
-    flex-wrap: wrap;
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
     gap: 2rem;
-    padding-bottom: 3rem;
+    padding-bottom: 1rem;
     @media (max-width: 930px) {
-      gap: 2rem;
+      grid-template-columns: 1fr;
     }
   }
 
   #skills-container .skill-card {
     border: 1px solid ${(props) => props.theme.primaryColor};
-    width: 22rem;
-    padding: 1rem 0;
+    padding: 1rem;
     text-align: center;
     transition: 0.5s ease-in-out;
-    &:hover {
-      transform: scale(1.1);
-    }
-    @media (max-width: 930px) {
-      width: 16rem;
-    }
+    min-height: 400px;
+    display: flex;
+    flex-direction: column;
+  }
+
+  #skills-container .skill-card:hover {
+    transform: scale(1.1);
   }
 
   #skills-container .skill-card h2 {
     color: aliceblue;
     font-size: 2rem;
     font-weight: 500;
-    padding-bottom: 1rem;
+    margin-bottom: 10px;
+    color: ${(props) => props.theme.secondaryColor};
     @media (max-width: 930px) {
       font-size: 1.5rem;
+      margin-bottom: 5px;
     }
   }
 
-  #skills-container .icon {
-    font-size: 9rem;
-    color: ${(props) => props.theme.secondaryColor};
+  #skills-container .skill-card p {
+    padding: 0.5rem 1rem; /* Ajuste o padding vertical e horizontal */
+    font-size: 16px; /* Aumenta o tamanho da fonte */
+    color: aliceblue;
+    margin-top: 20px; /* Ajusta a margem superior */
+    flex-grow: 1;
+    line-height: 1.6; /* Ajusta a altura da linha para melhor legibilidade */
+    max-width: 400px; /* Limita a largura do parágrafo */
+    margin-left: auto;
+    margin-right: auto;
+    text-align: center; /* Centraliza o texto */
 
     @media (max-width: 930px) {
-      font-size: 6rem;
+      margin-top: 10px;
     }
+  }
+
+  .button {
+    color: white;
+    font-size: 14px;
+    padding: 10px 20px;
+    margin-top: 20px; /* Adiciona margem superior ao botão */
+    cursor: pointer;
   }
 `;
 
