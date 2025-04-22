b77ccc75fdcf7812861d495891464f172134f260
Felipe serpa <felipe.mago_skt@hotmail.com>
2025-04-22 10:08:33 -0300

    implemetaçao d card efotos

--- src/sections/servicos/servicos.tsx
+++ src/sections/servicos/servicos.tsx
@@ -3,8 +3,15 @@ import styled from 'styled-components';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 
+import adesivo1 from '../../assets/images/adesivo vinil/adeisvo02.jpg';
+import adesivo2 from '../../assets/images/adesivo vinil/adesivo01.jpg';
+import adesivo3 from '../../assets/images/adesivo vinil/adesivo03.jpg';
+import adesivo4 from '../../assets/images/adesivo vinil/adesivo04.jpg';
+import adesivo5 from '../../assets/images/adesivo vinil/adesivo05.jpg';
+import adesivo6 from '../../assets/images/adesivo vinil/foto06.jpg';
+
 const servicos = [
-  { nome: 'Totens', fotos: ['/fotos/totens1.jpg', '/fotos/totens2.jpg'] },
+  { nome: 'Totens', fotos: ['', '/fotos/totens2.jpg'] },
   { nome: 'Banners', fotos: ['/fotos/banners1.jpg', '/fotos/banners2.jpg'] },
   { nome: 'Placas', fotos: ['/fotos/placas1.jpg', '/fotos/placas2.jpg'] },
   { nome: 'Fachadas', fotos: ['/fotos/fachadas1.jpg'] },
@@ -12,7 +19,10 @@ const servicos = [
   { nome: 'Letreiros', fotos: ['/fotos/letreiros1.jpg'] },
   { nome: 'Veiculos', fotos: ['/fotos/veiculos1.jpg'] },
   { nome: 'Stander', fotos: ['/fotos/stander1.jpg'] },
-  { nome: 'Adesivo Vinil', fotos: ['/fotos/adesivo1.jpg'] },
+  {
+    nome: 'Adesivo Vinil',
+    fotos: [adesivo1, adesivo2, adesivo3, adesivo4, adesivo5, adesivo6],
+  },
 ];
 
 export default function Servicos() {
@@ -60,10 +70,11 @@ const Container = styled.div`
   .conteudo {
     display: flex;
     gap: 3rem;
-    max-width: 1200px;
+    //max-width: 1200px;
     margin: 0 auto;
     align-items: flex-start;
     margin-top: 3rem; /* Espaço do Header */
+    justify-content: flex-end;
   }
 
   .cards {
@@ -71,7 +82,7 @@ const Container = styled.div`
     display: flex;
     flex-direction: column;
     gap: 1.2rem;
-    align-items: flex-start;
+    align-items: flex-end; /* Alinha os itens à direita */
   }
 
   .card {
@@ -98,7 +109,7 @@ const Container = styled.div`
   }
 
   .fotos {
-    width: 75%;
+    width: 80%;
   }
 
   .fotos h2 {
@@ -109,7 +120,7 @@ const Container = styled.div`
   .galeria {
     display: flex;
     flex-wrap: wrap;
-    gap: 1rem;
+    gap: 2rem;
   }
 
   .galeria img {
